e7c884bfc826f4205680a9d1c026c2f1e4a141a2
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2026-05-06 00:00:27 -0500

    feat: setup Electron main process with secure window and DB init

---
+++ src/main/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { initDB } = require('./database');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true, 
+      nodeIntegration: false,   
+    },
+    show: false, 
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(() => {
+  initDB();        
+  createWindow();  
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
